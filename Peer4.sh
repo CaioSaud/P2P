b98d550881afb59aa8f2f440dc1b91a62824b7a5
CaioSaud <caiosaud3@gmail.com>
2022-09-18 16:57:44 -0300

    Create Peer4.sh

---
+++ Peer4.sh
@@ -0,0 +1,3 @@
+freechains --host=localhost:8331 chain '$dict' post inline "O conceito de Quality Assurance (QA), que pode ser traduzido como “Garantia de Qualidade”, faz referência a um profissional ou uma equipe cuja função é garantir a qualidade no desenvolvimento de um produto ou serviço."
+freechains keys pubpvt 'senha5'
+freechains --host=localhost:8331 chain '$dict' like 1_C01784DE50210F887C23E7736D177799DFC24CA159D7424B92BE33E824E0A832 –sign=ABBFB2A3FB02ACB737A210A6A15BA716AA005B41245ACB56626426DBB4BF286E24F93AB92DDED5D272854BB2B46C517D63614ADE05FC4ED5DFBDF23D1C5B4968
