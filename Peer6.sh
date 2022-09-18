9d63604a05b3b908bae57ef237ba27a1939b2d28
CaioSaud <caiosaud3@gmail.com>
2022-09-18 16:58:14 -0300

    Create Peer6.sh

---
+++ Peer6.sh
@@ -0,0 +1,4 @@
+freechains --host=localhost:8332 chain '$dict' post inline "O termo Kanban é de origem japonesa e significa sinalização ou cartão, e propõe o uso de cartões (post-its) para indicar e acompanhar o andamento da produção dentro da indústria."
+freechains keys pubpvt 'senha10'
+freechains --host=localhost:8332 chain '$dict' consensus
+freechains --host=localhost:8332 chain '$dict' like 1_FCF8D85BFD69AA159C72E562531A7DF795930DA5EA1E793FB5F8C864D8086DE6 --sign=81BDACF7687FF1DF5B8A419D635700D2A4CAAB1A603EBCD39DA5F9F25141A76DC0AC6849151F992746C634C3A67CBCE42156AFDBB61777715496E7B025E2F0BA
