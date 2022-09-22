a5381bd5510c970583b2e0ff4f6aaae45bf0f6e5
CaioSaud <caiosaud3@gmail.com>
2022-09-22 13:57:48 -0300

    Update Peer5.sh

--- Peer5.sh
+++ Peer5.sh
@@ -1,5 +1,6 @@
-freechains --host=localhost:8332 chains join '$dict' DCFC1AB7A1D9B7377E9CC4067493C08617ED4F33744EE3E236B98F9D829A20ED
-C4D37E537CB321E40D71C79C320E2D6E19B520E8024C6D445C8B737A5B26E090
-freechains --host=localhost:8330 peer localhost:8332 send '$dict'
-freechains --host=localhost:8332 chain '$dict' consensus
-freechains --host=localhost:8332 chain '$dict' post inline "O sprint é uma ferramenta para atingir objetivos em um determinado período de tempo – de preferência, em ritmo acelerado. Para isso, ele ajuda a organizar o desenvolvimento de um projeto ou de um problema ao dividi-lo em tarefas menores que devem ser cumpridas normalmente ao longo de uma, duas ou três semanas."
+freechains --host=localhost:8332 chains join '#dict' ADB56B48005452626DA84219DF000A2F92F63DC533D76BE0B806C4CF84A422F8
+freechains --host=localhost:8330 peer localhost:8332 send '#dict'
+freechains --host=localhost:8332 chain '#dict' consensus
+freechains keys pubpvt 'qa’
+freechains --host=localhost:8332 chain '#dict' post --sign= ACD2157E61B9F45B3111EE337DCDE265BA9AA1BADB77C7DFDA95298A3BADE07BA156A703E6E9B9E2CA0FCCF940EB5D73D0015FF19106AB3C2035BC5D04820BC4 inline ‘Sprint – O sprint é uma ferramenta para atingir objetivos em um determinado período de tempo – de preferência, em ritmo acelerado. Para isso, ele ajuda a organizar o desenvolvimento de um projeto ou de um problema ao dividi-lo em tarefas menores que devem ser cumpridas normalmente ao longo de uma, duas ou três semanas."
+
