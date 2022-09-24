59195e408d00f1af1dbec7d1651a816a892e3706
CaioSaud <caiosaud3@gmail.com>
2022-09-24 10:45:42 -0300

    Update Peer4.sh

--- Peer4.sh
+++ Peer4.sh
@@ -1,4 +1,8 @@
-freechains keys pubpvt 'senior’
-freechains chain '#dict' post --sign= 17D2539687D6842106532939BF721313B7BE9138199D00C88282C5A725DC8D38634BEA9095659221A99E108AA09030B8136AE7479F7C06613352967A33BC38D9 inline ‘QA – O conceito de Quality Assurance (QA), que pode ser traduzido como “Garantia de Qualidade”, faz referência a um profissional ou uma equipe cuja função é garantir a qualidade no desenvolvimento de um produto ou serviço."
+freechains keys pubpvt 'senior'
+#criando a chave do usuário senior
+freechains chain '#dict' post --sign= 17D2539687D6842106532939BF721313B7BE9138199D00C88282C5A725DC8D38634BEA9095659221A99E108AA09030B8136AE7479F7C06613352967A33BC38D9 inline 'QA – O conceito de Quality Assurance (QA), que pode ser traduzido como “Garantia de Qualidade”, faz referência a um profissional ou uma equipe cuja função é garantir a qualidade no desenvolvimento de um produto ou serviço.'
+#post do usuário senior
 freechains --host=localhost:8331 chain '#dict' dislike 1_C01784DE50210F887C23E7736D177799DFC24CA159D7424B92BE33E824E0A832 –sign= 17D2539687D6842106532939BF721313B7BE9138199D00C88282C5A725DC8D38634BEA9095659221A99E108AA09030B8136AE7479F7C06613352967A33BC38D9
+#após o like recebi pelo pioneiro, e o tempo ter passado, o usuário senior já vai ter mais reputação e assim está ajudando a moderar o fórum.
 freechains chain '#dict' like 3_B2708C50798F09A06B9847DD335A51ED3BB4A73A4BA3E2482C4AE573FFC32D43 --sign=17D2539687D6842106532939BF721313B7BE9138199D00C88282C5A725DC8D38634BEA9095659221A99E108AA09030B8136AE7479F7C06613352967A33BC38D9
+#após mais tempo ter passado, o usuário senior já vai ter mais reputação e assim está ajudando a moderar o fórum.
